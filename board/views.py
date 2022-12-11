e9c8033e064dd39b3ed2e23daaa518a8fcfc3822
mijiFernandes <lkm366859@gmail.com>
2022-12-11 20:47:49 +0900

    db course #3

--- board/views.py
+++ board/views.py
@@ -19,6 +19,7 @@ def search(request):
         if cur.execute(f"SHOW TABLES LIKE '{table_name}';") == 0:
             return render(request, "search.html", {"data_set": [f"Table '{table_name}' doesn't exist"]})
         cur.execute(f"SELECT * FROM {table_name}")
+        db.close()
         return render(request, "search.html", {"data_set": cur.fetchall()})
     else:
         return render(request, "search.html")
@@ -34,11 +35,6 @@ def db(request):
 
 
 def undb(request):
-
-    # print(request.session.get('host'))
-    # print(request.session.get('user'))
-    # print(request.session.get('passwd'))
-    # print(request.session.get('db'))
     del request.session['host']
     del request.session['user']
     del request.session['passwd']
@@ -48,6 +44,35 @@ def undb(request):
 
 
 def csv(request):
+    if request.method == "POST":
+        db = MySQLdb.connect(host=request.session.get('host'),
+                             user=request.session.get('user'),
+                             passwd=request.session.get('passwd'),
+                             db=request.session.get('db'))
+
+        cur = db.cursor()
+        data = pd.read_csv(request.FILES['csv_file'], sep=',', header=None)
+        sql = "INSERT INTO `"
+        sql += str(request.FILES['csv_file'])[0:-4] + "` ("
+
+        temp = data.values
+        for i in temp[0]:
+            sql += f"`{i}`, "
+        sql = sql[:-2] + ") VALUES "
+        for i in range(1, len(temp)):
+            sql += "("
+            for j in temp[i]:
+                sql += f"'{j}', "
+            sql = sql[:-2] + "), "
+        sql = sql[:-2] + ";"
+        # print(sql)
+        # cur.execute("""INSERT INTO `1_fitness_measurement`
+        # (`PHONE_NUM`, `MAIL_ADDR`, `TEST_CNT`, `CENTER_NM`, `AGE_GBN`, `TEST_GBN`,
+        # `TEST_AGE`, `INPUT_GBN`, `CERT_GBN`, `TEST_YMD`, `TEST_SEX`) VALUES
+        # ('015-0019-9010', '04e8jlwv1qa6mxqrz2a@comcast.com', '2', 'KSPO송파', '성인', '일반', '29', '없음', '3등급', '20210908', 'M');""")
+        cur.execute(sql)
+        db.commit()
+        db.close()
     return render(request, "csv.html")
 
 
@@ -61,4 +86,6 @@ def schema(request):
 
         cur = db.cursor()
         cur.execute(f"{table_schema}")
+        db.commit()
+        db.close()
     return render(request, "schema.html")
