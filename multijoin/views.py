4d9d07338f961a324dd04cc9db865862ceaa2524
Yewon Lim <ga06033@yonsei.ac.kr>
2022-12-17 18:57:27 +0900

    221217 | multijoin select table

--- multijoin/views.py
+++ multijoin/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+import MySQLdb
 # Create your views here.
 
 def multijoin(request):
-    return render(request, 'multijoin/main.html')
+    if request.method == "POST":
+        table_name = request.POST.get('tablename')
+        db = MySQLdb.connect(host='localhost',
+                             user='root',
+                             passwd='yewon1108!',
+                             db='db_final')
+
+        cur = db.cursor()
+        cur.execute(f"SHOW TABLES LIKE '{table_name}%'")
+        db.close()
+        return render(request, 'multijoin/main.html', {"data_set":cur.fetchall(), "is_db": request.session.get('host')})
+    else:
+        return render(request, 'multijoin/main.html', {"is_db": request.session.get('host')})
