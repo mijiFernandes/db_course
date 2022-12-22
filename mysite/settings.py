f081807df9177816599ff996a33c18d24a9e0364
siyeolkim <cykim0528@gmail.com>
2022-12-22 21:47:40 +0900

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'finalDB',
+        'USER': 'root',
+        'PASSWORD': 'twice151020', # mariaDB 설치 시 입력한 root 비밀번호 입력
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
--- singlejoin/urls.py
+++ singlejoin/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.singlejoin_main, name='singlejoin_main'),
-    path('joinresult/', views.join, name='singlejoin_result'),
-    path('<str:table_name>', views.singlejoin, name='singlejoin'),
-
+    path('search/', views.singlejoin, name='singlejoin'),
+    path('process/', views.join, name='singlejoin_start'),
 ]
